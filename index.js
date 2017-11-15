41d33b978fbb457db5dddb5a89fda35d1461b0fe
Akkaash Goel <akkaashgoel@gmail.com>
2017-11-14 19:36:03 -0800

    :tada: integrate with woeid lambda and emit formatted alexa response

--- index.js
+++ index.js
@@ -4,8 +4,20 @@
 
 const Alexa = require('alexa-sdk');
 const TwitterWrapper = require('./twitter-wrapper');
+const rp = require('request-promise');
+const sprintf = require('sprintf-js').sprintf;
 
 const APP_ID = process.env.ALEXA_APP_ID;
+
+const API_GATEWAY_RESTAPI_ID = process.env.RESTAPI_ID;
+const API_GATEWAY_RESTAPI_REGION = process.env.RESTAPI_REGION;
+const API_GATEWAY_RESTAPI_STAGE = process.env.RESTAPI_STAGE === "Prod" ? "Prod" : "Stage";
+
+// https://{restapi_id}.execute-api.{region}.amazonaws.com/{stage_name}/
+const API_GATEWAY_URI = sprintf("https://%s.execute-api.%s.amazonaws.com/%s/",
+    API_GATEWAY_RESTAPI_ID,
+    API_GATEWAY_RESTAPI_REGION,
+    API_GATEWAY_RESTAPI_STAGE);
 
 const handlers = {
     'NewSession': function () {
@@ -21,19 +33,53 @@ const handlers = {
         if (citySlot && citySlot.value) {
             cityName = citySlot.value.toLowerCase();
         }
+        
+        const options = {
+            uri: process.env.OVERRIDE_RESTAPI_URI || API_GATEWAY_URI,
+            method: 'GET',
+            qs: {
+                city: cityName,
+                countryCode: 'US'
+            },
+            userAgent: 'Request-Promise',
+            json: true
+        }
 
-        TwitterWrapper.getTrends(cityName)
-            .then(data => {
-                const cardTitle = this.t('DISPLAY_CARD_TITLE', this.t('SKILL_NAME'), cityName);
-
-                const trendNames = data.map((trend) => trend.name);
-                this.attributes.speechOutput = this.t('RESULTS', cityName, trendNames);
-
-                this.emit(':tellWithCard', this.attributes.speechOutput, cardTitle, this.attributes.speechOutput);
-            })
-            .catch(error => {
+        rp(options)
+            .catch(err => {
+                console.log(err);
                 this.attributes.speechOutput = this.t('COULD_NOT_GET_TRENDS_MESSAGE');
                 this.emit(':tell', this.attributes.speechOutput);
+            })
+            .then(woeidApiResponse => {
+                if (woeidApiResponse) {
+                    const woeid = woeidApiResponse.woeid;
+                    TwitterWrapper.getTrends(woeid)
+                        .then(data => {
+                            const cardTitle = this.t('DISPLAY_CARD_TITLE', this.t('SKILL_NAME'), cityName);
+            
+                            const trendNames = data.map((trend, index) => {
+                                let trendOutput = trend.name + " ";
+                                if (index === (data.length - 1)) {
+                                    trendOutput = " and " + trendOutput;
+                                }
+
+                                return trendOutput;
+                            });
+                            const allTrends = trendNames.join();
+                            this.attributes.speechOutput = this.t('RESULTS', cityName, allTrends);
+            
+                            this.emit(':tellWithCard', this.attributes.speechOutput, cardTitle, this.attributes.speechOutput);
+                        })
+                        .catch(error => {
+                            this.attributes.speechOutput = this.t('COULD_NOT_GET_TRENDS_MESSAGE');
+                            this.emit(':tell', this.attributes.speechOutput);
+                        });
+
+                } else {
+                    this.attributes.speechOutput = this.t('COULD_NOT_GET_TRENDS_MESSAGE');
+                    this.emit(':tell', this.attributes.speechOutput);
+                }
             });
     },
     'AMAZON.HelpIntent': function () {
--- twitter-wrapper.js
+++ twitter-wrapper.js
@@ -18,17 +18,22 @@ const _getTwitter = () => {
 const WORLDWIDE_WOEID = 1;
 const TRENDS_PATH = 'trends/place';
 
-exports.getTrends = (cityName) => {
+const _getTopTrends = (data) => {
+    let topTrends = [];
+    data = data[0];
+    if (data && data.trends && data.trends.length) {
+        topTrends = data.trends.slice(0, 5);
+    }
+    return topTrends;
+}
+
+exports.getTrends = (woeid) => {
     return new Promise((resolveCallback, rejectCallback) => {
-        _getTwitter().get(TRENDS_PATH, { id: WORLDWIDE_WOEID }, (err, data, response) => {
+        _getTwitter().get(TRENDS_PATH, { id: woeid }, (err, data, response) => {
             if (err) {
                 rejectCallback(err);
             } else if (data) {
-                let topTrends = [];
-                data = data[0];
-                if (data && data.trends && data.trends.length) {
-                    topTrends = data.trends.slice(0, 5);
-                }
+                const topTrends = _getTopTrends(data);                
 
                 if (topTrends && topTrends.length) {
                     resolveCallback(topTrends);
